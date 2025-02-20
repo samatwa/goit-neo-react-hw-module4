d1de490298076861bd3b58f0a521bd7f635eccf6
ViacheslavKuznetsov <155986232+samatwa@users.noreply.github.com>
2025-02-20 17:04:04 +0100

    changes

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,40 +1,33 @@
-import { useState } from "react";
-import css from './SearchBar.module.css';
-import { FaSearch } from "react-icons/fa"; 
-import toast from "react-hot-toast";
+import { Formik, Form, Field } from "formik";
+import { FaSearch } from "react-icons/fa";
+import css from "./SearchBar.module.css";
 
 const SearchBar = ({ onSubmit }) => {
-  const [query, setQuery] = useState('');
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (query.trim() === '') {
-      toast.error("Please enter text to search images!");  
-      return;
-    }
-    onSubmit(query);
-    setQuery('');
-  };
-
   return (
     <header className={css.searchHeader}>
-      <form className={css.searchForm} onSubmit={handleSubmit}>
-        <FaSearch className={css.searchIcon} />
-        <input
-          type="text"
-          autoComplete="off"
-          autoFocus
-          placeholder="Search images and photos"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-          aria-label="Search images"
-          className={css.searchInput}
-        />
-      </form>
+      <Formik
+        initialValues={{ query: "" }}
+        onSubmit={(values, { resetForm }) => {
+          onSubmit(values.query);
+          resetForm();
+        }}
+      >
+        {({ submitForm }) => (
+          <Form className={css.searchForm}>
+            <FaSearch className={css.searchIcon} onClick={submitForm} />
+            <Field
+              type="text"
+              name="query"
+              autoComplete="off"
+              autoFocus
+              placeholder="Search images and photos"
+              className={css.searchInput}
+            />
+          </Form>
+        )}
+      </Formik>
     </header>
   );
-}
+};
 
-export default SearchBar
-
-
+export default SearchBar;
